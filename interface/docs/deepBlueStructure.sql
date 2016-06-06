9777abecc116b87de9612503e1476df3bfb05552
Jakob Bauer <github@jakobbauer.de>
2016-06-06 11:22:49 +0200

    modified sql structure, some documentation, python notebook for statistics

--- interface/docs/deepBlueStructure.sql
+++ interface/docs/deepBlueStructure.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `experiments` (
-  `_id` varchar(255) NOT NULL,
+  `experiment_id` varchar(255) NOT NULL,
   `name` text NOT NULL,
   `description` text NOT NULL,
   `genome` varchar(255) NOT NULL,
@@ -10,38 +10,19 @@ CREATE TABLE IF NOT EXISTS `experiments` (
   `data_type` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `format` text NOT NULL,
-  PRIMARY KEY (`_id`)
+  PRIMARY KEY (`experiment_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `sample_info` (
   `sample_id` varchar(255) NOT NULL,
   `key` varchar(255) NOT NULL,
   `value` text NOT NULL,
-  PRIMARY KEY (`sample_id`)
+  PRIMARY KEY (`sample_id`, `key`)
 );
 
 CREATE TABLE IF NOT EXISTS `extra_metadata` (
   `experiment_id` varchar(255) NOT NULL,
   `key` varchar(255) NOT NULL,
   `value` text NOT NULL,
-  PRIMARY KEY (`experiment_id`)
+  PRIMARY KEY (`experiment_id`, `key`)
 );
-
-
-
-/*'_id',
-'name'
-'description',
-'genome',
-'epigenetic_mark',
-'sample_id',
-'technique',
-'project',
-'data_type',
-'type',
-'format',
-
-'extra_metadata',
-'sample_info',
-'upload_info',
-'columns',*/
